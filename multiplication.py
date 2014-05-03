d25ce1b3727ed47aef1fe664b743580f051c0bae
Yawen Tian <tianyawen201209@hotmail.com>
2014-05-02 22:00:51 -0400

    Add merge sort

--- multiplication.py
+++ multiplication.py
@@ -1,7 +1,13 @@
 import math
 def karatuba (x, y):
-    ''' Python implementation of Karatuba algorithm
-    @params: x, y are n digits integers
+    ''' 
+    Python implementation of Karatuba algorithm
+    Idea drives from the dicussion of Karatuba algorithm in Coursera
+    Algorithms: Design and Analysis I(Stanford)
+    For details please refer to the video
+    @params: x, y are integers in string format
+    returns the multiplication of x, y
+    
     '''
     lenX = len(x)
     lenY = len(y)
@@ -9,6 +15,7 @@ def karatuba (x, y):
     if (lenX == lenY and lenX == 1):
         return int(x) * int(y)
     
+    # Balance x, y so that can be equally divided
     if (lenX > lenY):
         y = "0" + y
         half = int(lenX/2)
@@ -25,8 +32,54 @@ def karatuba (x, y):
     c, d = y[:half], y[half:]
     
     return math.pow(10, 2*(leng - half)) * karatuba(a, c) + math.pow(10, leng - half) * (karatuba(a, d) + karatuba(b, c)) + karatuba(b, d)
+
+def mergeSort(lst):
+    '''
+    Python implementation of recursive merge sort
+    '''
+    print "Splitting"
+    print lst
+    if (len(lst) > 1):
+        half = int(len(lst) / 2)
+        left = lst[:half]
+        right = lst[half:]
+        
+        mergeSort(left)
+        mergeSort(right)
+        
+        i = 0
+        j = 0
+        k = 0
+        
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                lst[k]=left[i]
+                i=i+1
+            else:
+                lst[k]=right[j]
+                j=j+1
+            k=k+1
+            print lst
+        
+        while i<len(left):
+            lst[k]=left[i]
+            i=i+1
+            k=k+1
+            print lst
+
+        while j<len(right):
+            lst[k]=right[j]
+            j=j+1
+            k=k+1  
+            print lst
+        
+    print "Merging"
+    print lst
     
 if __name__ == "__main__":
     x = "543"
-    y = "123"
+    y = "10"
     print karatuba(x, y)
+    alist = [54,26,93,17,77,31,44,55,20]
+    mergeSort(alist)
+    print alist
