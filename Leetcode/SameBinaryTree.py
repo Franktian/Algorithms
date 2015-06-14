e8a9abbc9e9d02b38ab5cabfadba36665821dcb3
Yawen Tian <tianyawen201209@hotmail.com>
2015-06-14 15:52:21 -0400

    Added solution to check same binary tree

---
+++ Leetcode/SameBinaryTree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} p
+    # @param {TreeNode} q
+    # @return {boolean}
+    def isSameTree(self, p, q):
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return not p and not q
